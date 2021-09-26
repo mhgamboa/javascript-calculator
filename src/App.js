6d56b527ca3827ba3f79fc871481f6a1f63674e2
Marcus <marcus.gamboa@gmail.com>
2021-09-25 18:01:55 -0600

    Add functionality for keyboard press

--- src/App.js
+++ src/App.js
@@ -13,11 +13,12 @@ class App extends React.Component {
     this.handleClear = this.handleClear.bind(this);
     this.handleOperator = this.handleOperator.bind(this);
     this.calculateExpression = this.calculateExpression.bind(this);
+    this.handleKeyboard = this.handleKeyboard.bind(this);
   }
 
   //Adds inputed Numbers to display
   handleNumber(e) {
-    let pushedButton = e.target.innerText;
+    let pushedButton = typeof e === "object" ? e.target.innerText : e;
 
     if (this.state.display === "0") {
       // If Display only shows 0 then erase 0 and show entered number
@@ -27,7 +28,7 @@ class App extends React.Component {
     } else if (pushedButton !== ".") {
       //If display not zero & pushed button is a number push inputed text to current display
       this.setState((state, props) => ({
-        display: `${this.state.display}${e.target.innerText}`,
+        display: `${this.state.display}${pushedButton}`,
       }));
       //Functionality if Decimal is pushed
     } else if (pushedButton === ".") {
@@ -41,7 +42,7 @@ class App extends React.Component {
       if (weAreOnTheFirstNumber) {
         if (!firstNumberHasDecimal) {
           this.setState((state, props) => ({
-            display: `${this.state.display}${e.target.innerText}`,
+            display: `${this.state.display}${pushedButton}`,
           }));
         }
         //If we're on the 2nd number check the 2nd number & see if there is a decimal & add it if needed
@@ -52,7 +53,7 @@ class App extends React.Component {
 
         if (!secondNumberHasDecimal) {
           this.setState((state, props) => ({
-            display: `${this.state.display}${e.target.innerText}`,
+            display: `${this.state.display}${pushedButton}`,
           }));
         }
       }
@@ -61,7 +62,7 @@ class App extends React.Component {
 
   //Triggers when addition/division/multiplicaiton/subtraction is clicked
   handleOperator(e) {
-    let pushedButton = e.target.innerText;
+    let pushedButton = typeof e === "object" ? e.target.innerText : e;
 
     let operationDoneReg = new RegExp(
       /^-*[0-9]*\.*[0-9]*[\+|\-|\/|\*|]-*[0-9]*\.*[0-9]*$/,
@@ -122,6 +123,23 @@ class App extends React.Component {
     });
   }
 
+  handleKeyboard(e) {
+    let numbersArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+    let operationArray = ["/", "*", "-", "+"];
+    if (numbersArray.includes(e.key)) {
+      this.handleNumber(e.key);
+    }
+    if (operationArray.includes(e.key)) {
+      this.handleOperator(e.key);
+    }
+    if (e.key === "Enter" || e.key === "=") {
+      this.calculateExpression();
+    }
+    if (e.key === "Escape") {
+      this.handleClear();
+    }
+  }
+
   componentDidMount() {
     //add functionality for number buttons
     const numberButtons = document.querySelectorAll(".numberButton");
@@ -133,6 +151,8 @@ class App extends React.Component {
     operatorButtons.forEach((button) => {
       button.addEventListener("click", this.handleOperator);
     });
+    // Add functionality for keyboard presses
+    document.addEventListener("keyup", this.handleKeyboard);
   }
 
   componentWillUnmount() {
@@ -146,6 +166,8 @@ class App extends React.Component {
     operatorButtons.forEach((button) => {
       button.removeEventListener("click", this.handleOperator);
     });
+
+    document.removeEventListener("keyup", this.handleKeyboard);
   }
 
   render() {
